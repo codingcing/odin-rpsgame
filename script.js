a1125204ec8c962da6580782f81d70c4fa8053b2
Ade Olugboji <a.olugboji7@gmail.com>
2022-07-25 20:15:13 +0100

    Fine tune skeletal functionality, style to come

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-let userChoice, computerChoice, userScore = 0, computerScore = 0, currentScore = 0, explan1 = "", explan2 = "";
-// currentScore will eventually tell us which of the computer and the user has won the most 'games' - each game is 5 rounds
+let userChoice, computerChoice, userScore = 0, computerScore = 0, currentScore = 0, explan1 = "", explan2 = "", roundOn = true, userWins = 0, computerWins = 0;
 
 updateHTML();
 
 document.getElementById("rock").addEventListener("click", () => {
     userChoice = "Rock";
     getComputerChoice();
-    getWinner(userChoice,computerChoice);
-    updateHTML();
+    getBattWinner(userChoice,computerChoice);
+    getRoundWinner();
+    explan1 = "";
+    explan2 = "";
 });
 document.getElementById("paper").addEventListener("click", () => {
     userChoice = "Paper";
     getComputerChoice();
-    getWinner(userChoice,computerChoice);
-    updateHTML();
+    getBattWinner(userChoice,computerChoice);
+    getRoundWinner();
+    explan1 = "";
+    explan2 = "";
 });
 document.getElementById("scissors").addEventListener("click", () => {
     userChoice = "Scissors";
     getComputerChoice();
-    getWinner(userChoice,computerChoice);
-    updateHTML();
+    getBattWinner(userChoice,computerChoice);
+    getRoundWinner();
+    explan1 = "";
+    explan2 = "";
 });
-// In future, we don't want this to be a function. The game should automatically begin when a user presses a button, and only reset
-// in case the score reaches 5
 
 function getComputerChoice(){
     let nonce = Math.floor(Math.random()*3)+1;
@@ -41,44 +44,32 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function getUserChoice(){
-    console.log(userChoice);
-}
-
-function getWinner(x,y){
+function getBattWinner(x,y){
     if (x === y && (x === "Rock" || x === "Paper" || x === "Scissors") && (y === "Rock" || y === "Paper" || y === "Scissors")) {
-        explan1 = "";
-        explan2 = "";
+        explan1 = "Draw!";
+        explan2 = "Draw!";
         return;
     } else if (x === "Rock" && y === "Scissors") {
-        // return `${x} wins`;
         userScore += 1;
         explan1 = "Rock BEATS Scissors!";
     } else if (x === "Scissors" && y === "Paper") {
-        // return `${x} wins`;
         userScore += 1;
         explan1 = "Scissors BEATS Paper!";
     } else if (x === "Paper" && y === "Rock") {
-        // return `${x} wins`;
         userScore += 1;
         explan1 = "Paper BEATS Rock!";
     } else if (y === "Rock" && x === "Scissors") {
-        // return `${y} wins`;
         computerScore += 1;
         explan2 = "Rock BEATS Scissors!";
     } else if (y === "Scissors" && x === "Paper") {
-        // return `${y} wins`;
         computerScore += 1;
         explan2 = "Scissors BEATS Paper!";
     } else if (y === "Paper" && x === "Rock") {
-        // return `${y} wins`;
         computerScore += 1;
         explan2 = "Paper BEATS Rock!";
     } else {
-        // return "One or both inputs are invalid..."
-    } 
-    // we want this function to also reset both of the values for x and y, and build into the game 
-    // the idea that both x and y must be defined for getWinner to run
+        return;
+    } return;
 }
 
 function updateHTML(){
@@ -86,7 +77,43 @@ function updateHTML(){
     (computerChoice)? document.getElementById("computerChoice").innerHTML = computerChoice : document.getElementById("computerChoice").innerHTML = "";
     (userScore)? document.getElementById("userScore").innerHTML = userScore : document.getElementById("userScore").innerHTML = 0;
     (computerScore)? document.getElementById("computerScore").innerHTML = computerScore : document.getElementById("computerScore").innerHTML = 0;
-    (currentScore)? document.getElementById("currentScore").innerHTML = currentScore : document.getElementById("currentScore").innerHTML = 0;
+    (userWins)? document.getElementById("userWins").innerHTML = userWins : document.getElementById("userWins").innerHTML = 0;
+    (computerWins)? document.getElementById("computerWins").innerHTML = computerWins : document.getElementById("computerWins").innerHTML = 0;
     (explan1)? document.getElementById("explan1").innerHTML = explan1 : document.getElementById("explan1").innerHTML = "";
     (explan2)? document.getElementById("explan2").innerHTML = explan2 : document.getElementById("explan2").innerHTML = "";
+}
+
+function newRound(){
+    userChoice = "";
+    computerChoice = "";
+    userScore = 0;
+    computerScore = 0;
+    roundOn = true;
+    explan1 = "";
+    explan2 = "";
+    document.getElementById("rock").removeAttribute("disabled");
+    document.getElementById("paper").removeAttribute("disabled");
+    document.getElementById("scissors").removeAttribute("disabled");
+}
+
+function getRoundWinner(){
+    if (userScore === 5){
+        userWins += 1;
+        document.getElementById("rock").setAttribute("disabled", true);
+        document.getElementById("paper").setAttribute("disabled", true);
+        document.getElementById("scissors").setAttribute("disabled", true);
+        updateHTML();
+        setTimeout(newRound,2000);
+        setTimeout(updateHTML,2001);
+    } else if (computerScore === 5){
+        computerWins += 1;
+        document.getElementById("rock").setAttribute("disabled", true);
+        document.getElementById("paper").setAttribute("disabled", true);
+        document.getElementById("scissors").setAttribute("disabled", true);
+        updateHTML();
+        setTimeout(newRound,2000);
+        setTimeout(updateHTML,2001);
+    } else{
+        updateHTML();
+    };
 }
